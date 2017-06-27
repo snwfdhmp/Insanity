0edc8c592f2985de813d16680bf06ce853521ab6
LVNDRY <monga.4landry@gmail.com>
2017-06-27 22:31:12 +0200

    English version

--- C/moreorless.c
+++ C/moreorless.c
@@ -9,20 +9,21 @@
 #define LEVEL3 1000
 #define MIN 1
 
-int niveau(){
-	unsigned short niveau;
+int level(){
+	unsigned short level;
 
-	printf("1--De 1 a 100\n");
-	printf("2-De 1 a 500\n");
-	printf("3-De 1 a 1000\n");
+	printf("1- 1 to 100\n");
+	printf("2- 1 to 500\n");
+	printf("3- 1 to 1000\n");
+	printf("\n");
 
 	do{
-		printf("Tapez un niveau de 1 a 3 uniquement :\n");
-		scanf("%d", &niveau);
+		printf("Enter the desired level (1 to 3 only) :\n");
+		scanf("%d", &level);
 		getchar();
-	}while(niveau < 1 || niveau > 3);
+	}while(level < 1 || level > 3);
 
-	return niveau;
+	return level;
 }
 
 int main(int argc, char const *argv[])
@@ -32,61 +33,63 @@ int main(int argc, char const *argv[])
 
 	srand(time(NULL));
 	
-	printf("====== BIENVENU DANS LE PLUS OU MOINS ======\n");
-	printf("Le but du jeu est de deviner le nombre auquel je pense\nVous etes pret ?\n");
-	printf("Tapez 'O' pour commencer\n");
+	printf("====== WELCOME TO THE PRICE IS RIGHT GAME ======\n");
+	printf("You have to guess what number I'm thinking about!\nAre you ready ?\n");
+	printf("Press 'O' to begin\n\n");
 
 	while(toupper(begin = getchar()) != 'O');
 	
 	do{
-		printf("Choisissez un niveau : \n");
-		int level = niveau();
+		printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		printf("Choose the level : \n");
+		int diff = level();
 		
-		if(level == 1){
+		if(diff == 1){
 			random = (rand() % (LEVEL1 - MIN + 1)) + MIN;
 			max = LEVEL1;
 		}
-		if(level == 2){
+		if(diff == 2){
 			random = (rand() % (LEVEL2- MIN + 1)) + MIN;
 			max = LEVEL2;
 		}
 
-		if(level == 3){
+		if(diff == 3){
 			random = (rand() % (LEVEL3 - MIN + 1)) + MIN;
 			max = LEVEL3;
 		}
 		
-		printf("Ok laisse moi reflechir....\n");
+		printf("Well let me think...\n");
 		sleep(2);
-		printf("C'est bon !\n");
+		printf("I have a number!\n\n");
 		//printf("%d\n", random);
 		
 		do{
-			printf("Tapez un nombre : ");
+			printf("Enter a number : ");
 			int check = scanf("%d", &player); //retourne 0 si c'est pas un chiffre
 			getchar();
 
 			while(!check){
-				printf("Que des chiffres! Je t'ai a l'oeil!!\n");
-				printf("Tapez un nombre : \n");
+				printf("Numbers only!!! Watch out!!\n");
+				printf("Enter a number \n");
 				getchar();
 				check = scanf("%d", &player);
 				
 			}
 			
 			if(player > random)
-				printf("Non je pense a un nombre plus petit!\n");
+				printf("No..Stop thinking so big!\n\n");
 
 			if(player < random)
-				printf("Voit les choses plus grand!\n");
+				printf("Nop see above!\n\n");
 
 			if(player == random)
 				finded = 1;
 		}while(finded == 0);
 
-		printf("Felicitation tu as trouver le nombre auquel je pense!!\n");
-		printf("Tu veux rejouer ?\n");
-		printf("Appuie sur 'Entree' pour recommence et 'q' pour quitter\n");
+		printf("\n\n\n");
+		printf("Congrats you found what number I was thinking about!! You are more intelligent than I though\n");
+		printf("You want to play again ?\n");
+		printf("Press 'Enter' to replay et 'q' to quit\n");
 	}while(getchar() != 'q' && getchar() == '\n');
 
 	return 0;
